e44dae87be781f78e499f7780f398dceefc12ef5
rownahGift <muhindorhona@gmail.com>
2022-07-03 06:00:28 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main - Entry point
+*main - entry point
 *Return: Always 0 (sucess)
 */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"programming is like bulding a multilingual puzzle");
 return (0);
 }
