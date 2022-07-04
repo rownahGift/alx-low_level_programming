eae0ff54a304fe2880e8057ae111656b354ceded
rownahGift <muhindorhona@gmail.com>
2022-07-04 06:26:33 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-*main - Print all possible combinations of single-digit numbers.
+*main - Prints all possible combinations of single-digit numbers.
 *
 *Return: Always 0.
 */
@@ -16,7 +16,7 @@ int main(void)
 			continue;
 
 		putchar(',');
-		pitchar('');
+		putchar('');
 	}
 
 	putchar('\n');
