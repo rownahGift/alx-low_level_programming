954b8f0f9e56100e43c5a636b53eaea3038b40da
rownahGift <muhindorhona@gmail.com>
2022-07-06 13:22:08 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,9 +19,9 @@ void print_to_98(int n)
 			else if (i == 98)
 				printf("%d\n", i);
 		}
-
-		else if (n >= 98)
-		{
+	}
+	else if (n >= 98)
+	{
 			for (j = n; j >= 98; j--)
 			{
 				if (j !=98)
@@ -29,6 +29,5 @@ void print_to_98(int n)
 				else if (j == 98)
 					printf("%d\n", j);
 			}
-		}
 	}
 }
