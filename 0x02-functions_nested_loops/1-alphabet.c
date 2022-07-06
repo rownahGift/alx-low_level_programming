41d4b7653d77d4057be83462baf839658eec22da
rownahGift <muhindorhona@gmail.com>
2022-07-06 09:35:24 -0700

    alpahbet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * main - print_alphabet - function to print abc
- *
- * Return: Always 0
- */
+*main - print_alphabet - function to print abc
+*
+*Return: Always 0
+*/
 void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-        {
+	{
 		_putchar(c);
-        }
+	}
 
-_putchar('\n');
+	_putchar('\n');
 }
