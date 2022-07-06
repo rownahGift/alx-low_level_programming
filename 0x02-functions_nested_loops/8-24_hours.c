debe29ac7ddd212a4bb0b581f13844227c19247f
rownahGift <muhindorhona@gmail.com>
2022-07-06 11:45:40 -0700

    mins

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,6 @@
  * jack_bauer - func that prints every minute of the day of Jack Bauer, starting 
  * from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
  * and resets mins
- * 
  * Return: 0
  */
 void jack_bauer(void)
