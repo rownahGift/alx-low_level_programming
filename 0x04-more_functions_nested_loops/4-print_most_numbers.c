46973486a7ac9dbc71457f25d4d62ab60ba59144
rownahGift <muhindorhona@gmail.com>
2022-07-08 02:36:30 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,16 +2,13 @@
 
 /**
  * print_most_numbers -> this prints most numbers
- *
- * Return: 0
  */
 void print_most_numbers(void)
 {
 	int x;
-
 	for (x = 0; x < 10; x++)
 	{
-		for (x != 2 && x != 4)
+		for ( x != 2 && x != 4)
 			_putchar(x + '0');
 	}
 	_putchar('\n');
