fc76be37f46dbd1b820562d07c5058bcc4b8c2d9
rownahGift <muhindorhona@gmail.com>
2022-07-08 01:31:18 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character to stdout
+ * @c: character to be printed
+ *
+ * Return: on success 1
+ *
+ * on error, -1 is returned, and error is set
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,18 +14,3 @@ void print_triangle(int size);
 void print_nu,ber(int n);
 
 #endif
-
-_putchar.c
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c:The character to print
- *
- * Return: on success 1.
- * on error, -1 is returned,and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
